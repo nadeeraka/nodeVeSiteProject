e774d166b916bd2b6ffa6b348686f702e8d5ec4a
nadeeraka <nimanthanadeeraka@outlook.com>
2018-11-10 17:52:52 +0530

    add ireas post add

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const exphbs  = require('express-handlebars');
-
+const bodyParser = require('body-parser');
 const {mongoose} = require('./db/db');
 const {Idea} = require('./model/Idea');
 
@@ -8,6 +8,9 @@ const app = express();
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 
 app.get('/',(req,res)=>
@@ -25,6 +28,47 @@ app.get('/about',(req,res)=>
 app.get('/ideas/add',(req,res)=>
 {
     res.render('ideas/add');
+})
+
+app.post('/ideas/add',(req,res)=>
+{
+    let errors = [];
+    let title = req.body.title;
+    let details = req.body.details;
+    console.log(JSON.stringify(req.body.title,undefined,2));
+   
+   if (title.length<2)
+     {
+        errors.push({ text: 'Title not valid !'})   
+     }
+    if (details.length < 2) {
+        errors.push({ text: 'Details not valid !' })
+    }
+    if (! title) {
+        errors.push({text:'Text is required'})
+    }
+    if (! details) {
+        errors.push({ text: 'Details : Details is required' })
+    }
+    // if (! typeOf(title) === "string") {
+    //     errors.push({ text: 'Dont use numbers !' });
+    // }
+    // if (!typeOf(details) === "string") {
+    //     errors.push({ text: "Dont use numbers !" });
+    // }
+
+    if (errors.length > 0)
+    {
+        res.render('ideas/add',{
+          errors,
+          title,
+          details
+      });   
+    }
+    else{
+        res.send('pass');
+    }
+
 })
 
 const Port = process.env.process || 3000;
